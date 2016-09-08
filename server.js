b4050ceb593620f44578519dd77ed4a4c086db7a
mucahitinel <mucahit.inel@hotmail.com>
2016-09-08 11:20:44 +0300

    Created a simple route on the server.

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/meows', function (req, res, next) {
+  var meows = [
+    'Hello I flipped over a cup.',
+    'My owner just said hi to me. Yum.',
+    'I ran around the house today and made a mess.',
+    'Just climbing around.',
+    'hello again'
+  ];
+  return res.send(meows);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
