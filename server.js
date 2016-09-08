b426b93dc86c27acd736c488e621a126f46a0925
mucahitinel <mucahit.inel@hotmail.com>
2016-09-08 14:49:33 +0300

    Storing data on the server side. but noDB

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-var express = require('express');
+var express = require('express'),
+        bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+
+var meows = [
+  'Hello I flipped over a cup.',
+  'My owner just said hi to me. Yum.',
+  'I ran around the house today and made a mess.',
+  'Just climbing around.',
+  'hello again there'
+];
+
 app.get('/meows', function (req, res, next) {
-  var meows = [
-    'Hello I flipped over a cup.',
-    'My owner just said hi to me. Yum.',
-    'I ran around the house today and made a mess.',
-    'Just climbing around.',
-    'hello again there'
-  ];
   return res.send(meows);
+});
+
+app.post('/meows',function (req, res, next) {
+  meows.push(req.body.newMeow);
+  res.send();
 });
 
 app.listen(3000, function () {
