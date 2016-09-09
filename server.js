546ef641baab7e8b6b0d9222da0c309562ca20b1
mucahitinel <mucahit.inel@hotmail.com>
2016-09-09 17:05:03 +0300

    push or get datas with mongoDb

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 var express = require('express'),
+        mongoClient = require('mongodb').MongoClient,
         bodyParser = require('body-parser');
 var app = express();
+
+var dbUrl = 'mongodb://localhost:27017/mittens',
+        db = null;
+
+mongoClient.connect(dbUrl, function (err, dbconn) {
+  if (!err) {
+    console.log('connected to Database');
+    /*
+    var adminDb = dbconn.admin();
+    adminDb.listDatabases(function (err, dbs) {
+      console.log(dbs.databases);
+    });
+    */
+    db = dbconn;
+  }
+});
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-
-var meows = [
-  'Hello I flipped over a cup.',
-  'My owner just said hi to me. Yum.',
-  'I ran around the house today and made a mess.',
-  'Just climbing around.',
-  'hello again there'
-];
-
 app.get('/meows', function (req, res, next) {
-  return res.send(meows);
+  db.collection('meows', function (err, meowsCollection) {
+    meowsCollection.find().toArray(function (err, meows) {
+      return res.json(meows);
+    });
+  });
 });
 
 app.post('/meows',function (req, res, next) {
-  meows.push(req.body.newMeow);
-  res.send();
+  db.collection('meows', function (err, meowsCollection) {
+    var newMeow = {
+      text: req.body.newMeow
+    };
+    meowsCollection.insert(newMeow, {w: 1}, function(err, meows){
+      console.log("Record added as "+ meows);
+      res.send();
+    });
+  });
 });
 
 app.listen(3000, function () {
