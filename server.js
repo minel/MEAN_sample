553210254fe466ee9b9bfa47a6f096c617809e54
mucahitinel <mucahit.inel@hotmail.com>
2016-09-25 21:04:01 +0300

    added bcryptjs for password hashing, and login functions activated

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
         mongoClient = require('mongodb').MongoClient,
         ObjectId = require('mongodb').ObjectId,
-        bodyParser = require('body-parser');
+        bodyParser = require('body-parser'),
+        bcrypt = require('bcryptjs');
 var app = express();
 
 var dbUrl = 'mongodb://localhost:27017/mittens',
@@ -23,6 +24,7 @@ mongoClient.connect(dbUrl, function (err, dbconn) {
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+//Get all meows from mittens DB
 app.get('/meows', function (req, res, next) {
   db.collection('meows', function (err, meowsCollection) {
     meowsCollection.find().toArray(function (err, meows) {
@@ -31,18 +33,20 @@ app.get('/meows', function (req, res, next) {
   });
 });
 
+//save a new Meow
 app.post('/meows',function (req, res, next) {
   db.collection('meows', function (err, meowsCollection) {
     var newMeow = {
       text: req.body.newMeow
     };
     meowsCollection.insert(newMeow, {w: 1}, function(err, meows){
-      console.log("Record added as "+ meows);
+      console.log("Record added as "+ newMeow);
       return res.send();
     });
   });
 });
 
+//remove a meow
 app.put('/meows/remove',function (req, res, next) {
   db.collection('meows', function (err, meowsCollection) {
     var meowId = req.body.meow._id;
@@ -53,11 +57,49 @@ app.put('/meows/remove',function (req, res, next) {
   });
 });
 
+//save a new user
 app.post('/users',function (req, res, next) {
   db.collection('users', function (err, usersCollection) {
-    usersCollection.insert(req.body, {w: 1}, function(err){
-      console.log("Record added as "+ req.body);
-      return res.send();
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+        var newUser = {
+          username: req.body.username,
+          password: hash
+        };
+        usersCollection.insert(newUser, {w: 1}, function(err){
+          console.log("Record added as "+ hash);
+          return res.send();
+        });
+      });
+    });
+  });
+});
+
+//simple login
+app.put('/users/signin',function (req, res, next) {
+  db.collection('users', function (err, usersCollection) {
+
+    usersCollection.findOne({username: req.body.username}, function (err, user) {
+      bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result) {
+          return res.send();
+        } else {
+          return res.status(400).send();
+        }
+      });
+    });
+
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+        var newUser = {
+          username: req.body.username,
+          password: hash
+        };
+        usersCollection.insert(newUser, {w: 1}, function(err){
+          console.log("Record added as "+ hash);
+          return res.send();
+        });
+      });
     });
   });
 });
