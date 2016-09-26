70e8b1c5380addceb695bba50d2a54485833e5cc
mucahitinel <mucahit.inel@hotmail.com>
2016-09-26 16:53:46 +0300

    generating jwt with jwt-simple, ng-cookies library added

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express'),
         mongoClient = require('mongodb').MongoClient,
         ObjectId = require('mongodb').ObjectId,
         bodyParser = require('body-parser'),
-        bcrypt = require('bcryptjs');
+        bcrypt = require('bcryptjs'),
+        jwt = require('jwt-simple');
 var app = express();
-
+var JWT_SECRET = 'catsmeow';
 var dbUrl = 'mongodb://localhost:27017/mittens',
         db = null;
 
@@ -80,26 +81,20 @@ app.put('/users/signin',function (req, res, next) {
   db.collection('users', function (err, usersCollection) {
 
     usersCollection.findOne({username: req.body.username}, function (err, user) {
-      bcrypt.compare(req.body.password, user.password, function (err, result) {
-        if (result) {
-          return res.send();
-        } else {
-          return res.status(400).send();
-        }
-      });
-    });
-
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(req.body.password, salt, function(err, hash) {
-        var newUser = {
-          username: req.body.username,
-          password: hash
-        };
-        usersCollection.insert(newUser, {w: 1}, function(err){
-          console.log("Record added as "+ hash);
-          return res.send();
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, function (err, result) {
+          if (result) {
+            var myToken = jwt.encode(user, JWT_SECRET);
+            return res.json({token: myToken});
+          } else {
+            return res.status(400).send();
+          }
         });
-      });
+      }
+      else {
+        return res.status(400).send();
+        console.log("An error occured on login");
+      }
     });
   });
 });
